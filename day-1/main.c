c8a7c9195990ae64b70cceb5847fe9d50f1f444e
Mohamed-Anes <89668631+Mohamed-Anes@users.noreply.github.com>
2021-12-04 16:07:12 +0100

    Add files via upload

---
+++ day-1/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int getline(FILE *my_file, char *str);
+
+int main()
+{
+    FILE *my_file = fopen("input.txt", "r");
+
+    char *str_depth = malloc(sizeof(char) * 5);
+    int depth[4], counter = 0, sum_1, sum_2;
+    int *ptr = depth;
+
+    for(int i = 0; i < 4; ++i){
+        getline(my_file, str_depth);
+        depth[i] = atoi(str_depth);
+        }
+    sum_1 = depth[0] + depth[1] + depth[2];
+    sum_2 = depth[1] + depth[2] + depth[3];
+
+    if(sum_1 < sum_2){
+            ++counter;
+        }
+
+
+    while((getline(my_file, str_depth)) == 0){
+
+        sum_1 -= depth[0];
+        sum_2 -= depth[1];
+
+        for(int i = 0; i < 3; ++i){
+            depth[i] = depth[i + 1];
+        }
+        depth[3] = atoi(str_depth);
+
+        sum_1 += depth[2];
+        sum_2 += depth[3];
+
+        if(sum_1 < sum_2){
+            ++counter;
+        }
+
+        }
+
+    printf("%d\n", counter);
+
+    fclose(my_file);
+    return 0;
+    }
+
+
+int getline(FILE *my_file, char *str){
+    char temp;
+    int i = 0;
+    while((temp = getc(my_file)) != '\n'){
+        if(temp == EOF){
+            return -1;
+            }
+        str[i++] = temp;
+        }
+    return 0;
+    }
